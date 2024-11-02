277c1a16b0f8773f548253180f5eb22371ebf145
mario <mario.caballero.948@gmail.com>
2024-11-02 19:17:00 -0300

    feat: add illness data structure and initialization procedures

--- src/app/vectores.pas
+++ src/app/vectores.pas
@@ -14,11 +14,18 @@ Const k = 14;
                                        'Vomitos','Dificultad para respirar',
                   'Sibilancia(sonido silbante y chillon durante la respiracion)'
                                       );
+  posiblesEnf = 14;
 
 Type 
   T_vector = array [1..k] Of boolean;
+  illnessData = Record
+    name: byte;
+    percentage: real;
+  End;
+  illnessArray = array [1..posiblesEnf] Of illnessData;
 
 Procedure inicializarv (Var v:T_vector);
+Procedure inicializarvPosibles (Var v:illnessArray);
 Procedure consulta_sintomas(Var v:t_vector);
 
 Implementation
@@ -29,6 +36,16 @@ Var i: 1..k;
 Begin
   For i:=1 To k Do
     v[i] := false;
+End;
+Procedure inicializarvPosibles (Var v:illnessArray);
+
+Var i: 1..posiblesEnf;
+Begin
+  For i:=1 To posiblesEnf Do
+    Begin
+      v[i].name := 0;
+      v[i].percentage := 0;
+    End;
 End;
 Procedure consulta_sintomas(Var v:t_vector);
 
